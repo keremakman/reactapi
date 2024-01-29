ce235afb2347d9a8054d436891f1bc757a3ecde5
keremakman272727 <52568414+keremakman272727@users.noreply.github.com>
2024-01-29 16:20:56 +0300

    project create

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+function About(){
+    return (
+        <div>
+             <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4> About Us
+                            </h4>
+                        </div>
+                        <div className="card-body">
+                            <p>Hoş Geldiniz! Biz WebYazılım, 2024 yılında kurulan bir WEB API firmasıyız. Misyonumuz, müşterilerimize kaliteli ürünler ve hizmetler sunarak onların ihtiyaçlarını karşılamak ve memnuniyetini sağlamaktır.
+                            Her geçen gün değişen ve gelişen dünyada, müşterilerimize en iyi hizmeti sunmayı hedefliyoruz. En son teknoloji ve en iyi uygulamaları kullanarak, müşterilerimizin beklentilerini aşmayı amaçlıyoruz.
+                            Kalite ve Güvenilirlik: WebYazılım, ürün ve hizmetlerinde kaliteye ve güvenilirliğe büyük önem verir. Müşteri memnuniyetini en üst seviyede tutarak uzun vadeli ilişkiler kurmayı amaçlarız.
+
+Uzman Ekibimiz: Alanında uzmanlaşmış bir ekip, müşterilerimize en iyi hizmeti sunmak için sürekli olarak çalışır. Deneyimli ve yetenekli ekibimiz, her projeye profesyonellik ve özveri ile yaklaşır.
+
+İnovasyon ve Gelişim: WebYazılım, sektördeki yenilikleri takip eder ve sürekli olarak kendini geliştirir. Bu sayede müşterilerimize en güncel ve etkili çözümleri sunabilmekteyiz.
+
+Sosyal Sorumluluk: Sadece iş yapmakla kalmayıp, topluma ve çevreye karşı sorumluluklarımızın bilincindeyiz. Sosyal sorumluluk projelerine katkıda bulunarak toplumsal fayda sağlamayı amaçlıyoruz.
+                            </p>
+                            Teşekkür ederiz!
+
+                            <br/>WebYazılım Ekibi
+                            <hr/>
+                            <br/>
+                            <p>&copy; 2024 | Tüm hakları saklıdır. </p>
+                            <hr/>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
